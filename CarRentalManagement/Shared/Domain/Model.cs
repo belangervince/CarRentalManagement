66450e5aed7a10416831ec76a08c7c3afeaf8380
belangervince <belangervince@outlook.com>
2021-05-29 17:00:07 -0400

    removed usings

--- CarRentalManagement/Shared/Domain/Model.cs
+++ CarRentalManagement/Shared/Domain/Model.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CarRentalManagement.Shared.Domain
+﻿namespace CarRentalManagement.Shared.Domain
 {
     public class Model : BaseDomainModel
     {

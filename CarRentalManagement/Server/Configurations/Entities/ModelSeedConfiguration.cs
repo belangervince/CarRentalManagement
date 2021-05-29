4fc76e44633afa7501e3068df216995b73b1cb74
belangervince <belangervince@outlook.com>
2021-05-29 19:54:13 -0400

    model seed

---
+++ CarRentalManagement/Server/Configurations/Entities/ModelSeedConfiguration.cs
@@ -0,0 +1,47 @@
+﻿using CarRentalManagement.Shared.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace CarRentalManagement.Server.Configurations.Entities
+{
+    public class ModelSeedConfiguration : IEntityTypeConfiguration<Model>
+    {
+        public void Configure(EntityTypeBuilder<Model> builder)
+        {
+            builder.HasData(
+                new Model
+                {
+                    CreatedBy = "System",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    Name = "Prius",
+                    UpdatedBy = "System"
+                },
+                new Model
+                {
+                    CreatedBy = "System",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    Name = "Vitz",
+                    UpdatedBy = "System"
+                },
+                new Model
+                {
+                    CreatedBy = "System",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    Name = "3 Series",
+                    UpdatedBy = "System"
+                },
+                new Model
+                {
+                    CreatedBy = "System",
+                    DateCreated = DateTime.Now,
+                    DateUpdated = DateTime.Now,
+                    Name = "X5",
+                    UpdatedBy = "System"
+                });
+        }
+    }
+}

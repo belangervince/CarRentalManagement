424947f4d349092451b31298c1faba0937e80d16
belangervince <belangervince@outlook.com>
2021-05-29 21:53:48 -0400

    VehiclesController

---
+++ CarRentalManagement/Server/Controllers/VehiclesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarRentalManagement.Server.Data;
+using CarRentalManagement.Shared.Domain;
+using CarRentalManagement.Server.IRepository;
+
+namespace CarRentalManagement.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VehiclesController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public VehiclesController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Vehicles
+        [HttpGet]
+        public async Task<IActionResult> GetVehicles()
+        {
+            var Vehicles = await _unitOfWork.Vehicles.GetAll();
+            return Ok(Vehicles);
+        }
+
+        // GET: api/Vehicles/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMake(int id)
+        {
+            var make = await _unitOfWork.Vehicles.Get(q => q.Id == id);
+
+            if (make == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(make);
+        }
+
+        // PUT: api/Vehicles/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMake(int id, Vehicle vehicle)
+        {
+            if (id != vehicle.Id)
+            {
+                return BadRequest();
+            }
+
+            _unitOfWork.Vehicles.Update(vehicle);
+
+            try
+            {
+                await _unitOfWork.Save(HttpContext);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!(await MakeExists(id)))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Vehicles
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Make>> PostMake(Vehicle vehicle)
+        {
+            await _unitOfWork.Vehicles.Insert(vehicle);
+            await _unitOfWork.Save(HttpContext);
+
+            return CreatedAtAction("GetMake", new { id = vehicle.Id }, vehicle);
+        }
+
+        // DELETE: api/Vehicles/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMake(int id)
+        {
+            var make = await _unitOfWork.Vehicles.Get(q => q.Id == id);
+            if (make == null)
+            {
+                return NotFound();
+            }
+            await _unitOfWork.Vehicles.Delete(id);
+            await _unitOfWork.Save(HttpContext);
+
+            return NoContent();
+        }
+
+        private async Task<bool> MakeExists(int id)
+        {
+            var make = await _unitOfWork.Vehicles.Get(q => q.Id == id);
+            return make != null;
+        }
+    }
+}

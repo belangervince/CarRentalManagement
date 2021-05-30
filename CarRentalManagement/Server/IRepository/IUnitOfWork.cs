77d931b64f779b4f0d14cd8dd44b5a857d406fb3
belangervince <belangervince@outlook.com>
2021-05-29 21:07:40 -0400

    IUnitOfWork

---
+++ CarRentalManagement/Server/IRepository/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using CarRentalManagement.Shared.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CarRentalManagement.Server.IRepository
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<Make> Makes { get; set; }
+        //IGenericRepository<Model> Models { get; set; }
+        //IGenericRepository<Vehicle> Vehicles { get; set; }
+    }
+}

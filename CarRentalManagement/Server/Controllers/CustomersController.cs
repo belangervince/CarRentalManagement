1799f3f63d40222a1b42455ac1aa53a2458f99ac
belangervince <belangervince@outlook.com>
2021-05-29 21:57:50 -0400

    CustomersController

---
+++ CarRentalManagement/Server/Controllers/CustomersController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarRentalManagement.Server.Data;
+using CarRentalManagement.Shared.Domain;
+using CarRentalManagement.Server.IRepository;
+
+namespace CarRentalManagement.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CustomersController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Customers
+        [HttpGet]
+        public async Task<IActionResult> GetCustomers()
+        {
+            var Customers = await _unitOfWork.Customers.GetAll();
+            return Ok(Customers);
+        }
+
+        // GET: api/Customers/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMake(int id)
+        {
+            var make = await _unitOfWork.Customers.Get(q => q.Id == id);
+
+            if (make == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(make);
+        }
+
+        // PUT: api/Customers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMake(int id, Customer customer)
+        {
+            if (id != customer.Id)
+            {
+                return BadRequest();
+            }
+
+            _unitOfWork.Customers.Update(customer);
+
+            try
+            {
+                await _unitOfWork.Save(HttpContext);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!(await MakeExists(id)))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Customers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Make>> PostMake(Customer customer)
+        {
+            await _unitOfWork.Customers.Insert(customer);
+            await _unitOfWork.Save(HttpContext);
+
+            return CreatedAtAction("GetMake", new { id = customer.Id }, customer);
+        }
+
+        // DELETE: api/Customers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMake(int id)
+        {
+            var make = await _unitOfWork.Customers.Get(q => q.Id == id);
+            if (make == null)
+            {
+                return NotFound();
+            }
+            await _unitOfWork.Customers.Delete(id);
+            await _unitOfWork.Save(HttpContext);
+
+            return NoContent();
+        }
+
+        private async Task<bool> MakeExists(int id)
+        {
+            var make = await _unitOfWork.Customers.Get(q => q.Id == id);
+            return make != null;
+        }
+    }
+}

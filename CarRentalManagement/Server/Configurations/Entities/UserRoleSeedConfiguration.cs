9d83bd462be7aa91a54816e74e79e1cd12d5ac73
belangervince <belangervince@outlook.com>
2021-05-29 20:14:01 -0400

    UserRoleSeedConfiguration

---
+++ CarRentalManagement/Server/Configurations/Entities/UserRoleSeedConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using CarRentalManagement.Server.Models;
+using CarRentalManagement.Shared.Domain;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace CarRentalManagement.Server.Configurations.Entities
+{
+    public class UserRoleSeedConfiguration : IEntityTypeConfiguration<IdentityUserRole<string>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityUserRole<string>> builder)
+        {
+            builder.HasData(
+                new IdentityUserRole<string>
+                {
+                    RoleId = "a9394b64-77ed-4f53-8127-50cc61a5ca24",
+                    UserId = "b9394b64-77ed-4f53-8127-50cc61a5ca23"
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = "a9394b64-77ed-4f53-8127-50cc61a5ca23",
+                    UserId = "b8394b64-77ed-4f53-8127-50cc61a5ca23"
+                });
+        }
+    }
+}
--- CarRentalManagement/Server/Data/ApplicationDbContext.cs
+++ CarRentalManagement/Server/Data/ApplicationDbContext.cs
@@ -36,6 +36,7 @@ namespace CarRentalManagement.Server.Data
             builder.ApplyConfiguration(new ModelSeedConfiguration());
             builder.ApplyConfiguration(new RoleSeedConfiguration());
             builder.ApplyConfiguration(new UserSeedConfiguration());
+            builder.ApplyConfiguration(new UserRoleSeedConfiguration());
         }
     }
 }

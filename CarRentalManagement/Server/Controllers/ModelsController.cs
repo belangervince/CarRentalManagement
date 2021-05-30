d8d429c6af09b234eef197312046d6a683c03d91
belangervince <belangervince@outlook.com>
2021-05-29 21:51:43 -0400

    ModelsController

---
+++ CarRentalManagement/Server/Controllers/ModelsController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CarRentalManagement.Server.Data;
+using CarRentalManagement.Shared.Domain;
+using CarRentalManagement.Server.IRepository;
+
+namespace CarRentalManagement.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelsController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ModelsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        // GET: api/Models
+        [HttpGet]
+        public async Task<IActionResult> GetModels()
+        {
+            var Models = await _unitOfWork.Models.GetAll();
+            return Ok(Models);
+        }
+
+        // GET: api/Models/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMake(int id)
+        {
+            var make = await _unitOfWork.Models.Get(q => q.Id == id);
+
+            if (make == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(make);
+        }
+
+        // PUT: api/Models/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMake(int id, Model model)
+        {
+            if (id != model.Id)
+            {
+                return BadRequest();
+            }
+
+            _unitOfWork.Models.Update(model);
+
+            try
+            {
+                await _unitOfWork.Save(HttpContext);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!(await MakeExists(id)))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Models
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Make>> PostMake(Model model)
+        {
+            await _unitOfWork.Models.Insert(model);
+            await _unitOfWork.Save(HttpContext);
+
+            return CreatedAtAction("GetMake", new { id = model.Id }, model);
+        }
+
+        // DELETE: api/Models/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMake(int id)
+        {
+            var make = await _unitOfWork.Models.Get(q => q.Id == id);
+            if (make == null)
+            {
+                return NotFound();
+            }
+            await _unitOfWork.Models.Delete(id);
+            await _unitOfWork.Save(HttpContext);
+
+            return NoContent();
+        }
+
+        private async Task<bool> MakeExists(int id)
+        {
+            var make = await _unitOfWork.Models.Get(q => q.Id == id);
+            return make != null;
+        }
+    }
+}

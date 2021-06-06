b612b79d65074058cd0b0709c2efd2cdbd3c7274
belangervince <belangervince@outlook.com>
2021-06-05 20:35:11 -0400

    http interceptor for global error handling

--- CarRentalManagement/Client/Pages/Colours/Index.razor.cs
+++ CarRentalManagement/Client/Pages/Colours/Index.razor.cs
@@ -1,4 +1,5 @@
-﻿using CarRentalManagement.Client.Static;
+﻿using CarRentalManagement.Client.Services;
+using CarRentalManagement.Client.Static;
 using CarRentalManagement.Shared.Domain;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
@@ -11,23 +12,28 @@ using System.Threading.Tasks;
 
 namespace CarRentalManagement.Client.Pages.Colours
 {
-    public partial class Index
+    public partial class Index : IDisposable
     {
         [Inject] 
         HttpClient _client { get; set; }
 
-        [Inject] 
+        [Inject]
         IJSRuntime _js { get; set; }
+
+        [Inject]
+        HttpInterceptorService _interceptor { get; set; }
 
         private List<Colour> Colours;
 
         protected async override Task OnInitializedAsync()
         {
+            _interceptor.MonitorEvent();
             Colours = await _client.GetFromJsonAsync<List<Colour>>($"{Endpoints.ColoursEndpoint}");
         }
 
         async Task Delete(int id)
         {
+            _interceptor.MonitorEvent();
             var colour = Colours.First(q => q.Id == id);
             var confirm = await _js.InvokeAsync<bool>("confirm", $"Do you want to delete {colour.Name}?");
             if (confirm)
@@ -35,6 +41,11 @@ namespace CarRentalManagement.Client.Pages.Colours
                 await _client.DeleteAsync($"{Endpoints.ColoursEndpoint}{id}");
                 await OnInitializedAsync();
             }
+        }
+
+        public void Dispose()
+        {
+            _interceptor.DisposeEvent();
         }
     }
 }
---
+++ CarRentalManagement/Client/Services/HttpInterceptorService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using Toolbelt.Blazor;
+
+namespace CarRentalManagement.Client.Services
+{
+    public class HttpInterceptorService
+    {
+        private readonly HttpClientInterceptor interceptor;
+        private readonly NavigationManager navigationManager;
+
+        public HttpInterceptorService(HttpClientInterceptor interceptor, NavigationManager navigationManager)
+        {
+            this.interceptor = interceptor;
+            this.navigationManager = navigationManager;
+        }
+
+        public void MonitorEvent() => this.interceptor.AfterSend += InterceptResponse;
+
+        public void DisposeEvent() => this.interceptor.AfterSend -= InterceptResponse;
+
+        private void InterceptResponse(object sender, HttpClientInterceptorEventArgs e)
+        {
+            string message = string.Empty;
+            if (!e.Response.IsSuccessStatusCode)
+            {
+                var responseCode = e.Response.StatusCode;
+                switch (responseCode)
+                {
+                    case System.Net.HttpStatusCode.NotFound:
+                        navigationManager.NavigateTo("/404");
+                        //message = "The requested resource was not found.";
+                        break;
+                    case System.Net.HttpStatusCode.Unauthorized:
+                    case System.Net.HttpStatusCode.Forbidden:
+                        navigationManager.NavigateTo("/Unauthorized");
+                        //message = "The requested resource was not found.";
+                        break;
+                    default:
+                        navigationManager.NavigateTo("/500");
+                        //message = "Something went wrong, please contact Administrator.";
+                        break;
+                }
+            }
+        }
+    }
+}
